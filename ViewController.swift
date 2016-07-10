367988e89157bbf7b8c0106523ec971b2e7c8a46
矢野史洋 <a2.mj.pop@gmail.com>
2016-07-10 15:32:03 +0900

    add IBOutletで繋げた

--- ViewController.swift
+++ ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var emailField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    @IBOutlet weak var loginButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
@@ -19,15 +23,7 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func login(sender: AnyObject) {
+        print("login")
     }
-    */
-
 }
