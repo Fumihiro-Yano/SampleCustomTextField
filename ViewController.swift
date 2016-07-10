5555077dadcf1ba01e816200236f3b689eb6b293
矢野史洋 <a2.mj.pop@gmail.com>
2016-07-10 15:33:07 +0900

    add , UITextFieldDelegateを追加

--- ViewController.swift
+++ ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate{
 
     @IBOutlet weak var emailField: UITextField!
     @IBOutlet weak var passwordField: UITextField!
